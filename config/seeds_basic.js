53f0e950f36a89b656cff9acdbee5e3733f988c4
Gus Nordhielm <gnordhielm@gmail.com>
2017-04-11 15:57:13 -0700

    alter seeds for cml

--- config/seeds_basic.js
+++ config/seeds_basic.js
@@ -4,6 +4,7 @@ var recipes = [
 		subtitle: 'A great family dinner',
 		attribution: 'Allrecipes.com',
 		is_public: true,
+		is_cml: true,
 		created_at: 12345678,
 		body: "6 skinless, boneless chicken breast halves\n 1/4 cup all-purpose flour\n 1/2 teaspoon salt\n 1 pinch ground black pepper\n 3 tablespoons butter\n 1 (14.5 ounce) can stewed tomatoes, with liquid\n 1/2 cup water\n 2 tablespoons brown sugar\n 2 tablespoons distilled white vinegar\n 2 tablespoons Worcestershire sauce\n 1 teaspoon salt\n 2 teaspoons chili powder\n 1 teaspoon mustard powder\n 1/2 teaspoon celery seed\n 1 clove garlic, minced\n 1/8 teaspoon hot pepper sauce\n\nIn a shallow dish or bowl, combine flour, 1/2 teaspoon salt and ground black pepper. Coat chicken breasts with flour mixture. Melt butter in a large skillet over medium heat, and brown chicken on all sides.\n Remove from skillet, and drain on paper towels.In the same skillet, combine the tomatoes, water, brown sugar, vinegar and Worcestershire sauce. Season with salt, chili powder, mustard, celery seed, garlic and hot pepper sauce.\n Bring to a boil; reduce heat, and return chicken to skillet. Cover, and simmer for 35 to 40 minutes, or until chicken is tender, no longer pink and juices run clear."
 	},
