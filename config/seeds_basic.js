71d6614d13529c5b60860db910287735ae03d3fe
Gus Nordhielm <gnordhielm@gmail.com>
2017-04-13 14:29:00 -0700

    add to seeds file

--- config/seeds_basic.js
+++ config/seeds_basic.js
@@ -29,11 +29,53 @@ var recipes = [
 	},
 	{
 		title: 'Roasted Potatoes and Asparagus',
+		subtitle: 'Healthy and hearty site.',
 		attribution: 'Allrecipes.com',
 		is_public: true,
 		created_at: 12345678,
 		image_url: 'http://images.media-allrecipes.com/userphotos/560x315/3293486.jpg',
 		body: "1 1/2 pounds red potatoes, cut into chunks\n 2 tablespoons extra virgin olive oil\n 8 cloves garlic, thinly sliced\n 4 teaspoons dried rosemary\n 4 teaspoons dried thyme\n 2 teaspoons kosher salt\n 1 bunch fresh asparagus, trimmed and cut into 1 inch pieces\n ground black pepper to taste\n\nPreheat oven to 425 degrees F (220 degrees C).\n In a large baking dish, toss the red potatoes with 1/2 the olive oil, garlic, rosemary, thyme, and 1/2 the kosher salt. Cover with aluminum foil.\n Bake 20 minutes in the preheated oven. Mix in the asparagus, remaining olive oil, and remaining salt. Cover, and continue cooking 15 minutes, or until the potatoes are tender. Increase oven temperature to 450 degrees F (230 degrees C). Remove foil, and continue cooking 5 to 10 minutes, until potatoes are lightly browned. Season with pepper to serve."
+	},
+	
+	{
+		title: 'Sweet and Spicy Chicken',
+		subtitle: 'Rollercoaster for the mouth.',
+		attribution: 'Allrecipes.com',
+		is_public: true,
+		is_cml: true,
+		created_at: 12345678,
+		image_url: 'http://images.media-allrecipes.com/userphotos/560x315/1655056.jpg',
+		body: "1 tablespoon brown sugar\n 2 tablespoons honey\n 1/4 cup soy sauce\n 2 teaspoons chopped fresh ginger root\n 2 teaspoons chopped garlic\n 2 tablespoons hot sauce\n salt and pepper to taste\n 4 skinless, boneless chicken breast halves - cut into 1/2 inch strips\n 1 tablespoon vegetable oil\n Directions\n Mix together brown sugar, honey, soy sauce, ginger, garlic and hot sauce in a small bowl.\n Lightly salt and pepper the chicken strips.\n Heat oil in a large skillet over medium heat. Add chicken strips and brown on both sides, about 1 minute per side. Pour the sauce over the chicken. Simmer uncovered until the sauce thickens, 8 to 10 minutes.\n"
+
+	},
+	{
+		title: 'Buttery Cake Squares',
+		subtitle: 'An indulgent, gratifying dessert',
+		attribution: 'Allrecipes.com',
+		is_public: true,
+		created_at: 91234783,
+		image_url: 'http://images.media-allrecipes.com/userphotos/560x315/4138906.jpg',
+		body: "Cake Base:\n 4 ounces Finlandia Unsalted Butter, softened\n 1 egg\n 3 tablespoons milk\n 1/2 tablespoon vanilla extract\n 1/2 cup flour\n 1 box yellow cake mix\n Buttery Sweet \n Topping:\n 8 ounces Finlandia Unsalted Butter, softened\n 8 ounces cream cheese, softened\n 8 ounces cake flour\n 1 pound powdered sugar\n 3/4 teaspoon salt\n 2 teaspoons vanilla extract\n 2 eggs\n 1/2 cup corn syrup\n 2 tablespoons milk\n Directions\n Preheat oven to 350 degrees F. Grease 9x13-inch pan with Finlandia Unsalted Butter.\n Begin by making the cake base. Mix Finlandia Unsalted Butter, egg, milk, vanilla, flour and cake mix in a mixing bowl fitted with the paddle attachment until just combined. Press into the bottom of the 9 x 13 cake pan.\n In the same bowl start making your topping. Mix Finlandia Unsalted Butter, cream cheese and vanilla until smooth. Slowly add in powdered sugar, cake flour and salt.\n Once the mixture starts to come together, add in the eggs, corn syrup and milk. Mix on medium speed for three minutes until smooth, light and fluffy.\n Spread evenly over the cake base and place into the oven. Bake until the sides are golden brown and the middle is just set and lightly golden (approximately 35 to 45 minutes). Let cool.\n Once the butter cake has cooled, sprinkle the top with powdered sugar and dig in!\n "
+	},
+	{
+		title: 'Cabbage Soup',
+		subtitle: 'Diet food',
+		attribution: 'Allrecipes.com',
+		is_public: true,
+		created_at: 12345678,
+		image_url: 'http://images.media-allrecipes.com/userphotos/250x250/3086983.jpg',
+		body: "5 carrots, chopped\n 3 onions, chopped\n 2 (16 ounce) cans whole peeled tomatoes, with liquid\n 1 large head cabbage, chopped\n 1 (1 ounce) envelope dry onion soup mix\n 1 (15 ounce) can cut green beans, drained\n 2 quarts tomato juice\n 2 green bell peppers, diced\n 10 stalks celery, chopped\n 1 (14 ounce) can beef broth\n Directions\n Place carrots, onions, tomatoes, cabbage, green beans, peppers, and celery in a large pot. Add onion soup mix, tomato juice, beef broth, and enough water to cover vegetables. Simmer until vegetables are tender. May be stored in the refrigerator for several days.\n"
+	
+
+	},
+	{
+		title: 'Wake-Up Casserole',
+		attribution: 'Allrecipes.com',
+		subtitle: 'Breakfast of champs',
+		is_public: true,
+		created_at: 12345678,
+		image_url: 'http://images.media-allrecipes.com/userphotos/560x315/1711439.jpg',
+		body: "8 frozen hash brown patties\n 4 cups shredded Cheddar cheese\n 1 pound cooked ham, cut into cubes\n 7 eggs\n 1 cup milk\n 1/2 teaspoon salt\n 1/2 teaspoon ground mustard\n Directions\n Preheat oven to 350 degrees F (175 degrees C). Grease a 9x13-inch baking dish.\n Arrange hash brown patties on the prepared baking dish. Sprinkle Cheddar cheese and ham over patties.\n Beat eggs, milk, salt, and mustard in a large bowl using a whisk; pour over ham. Cover baking dish with aluminum foil.\n Bake in the preheated oven for 1 hour. Remove foil and bake until edges are golden brown and a knife inserted in the center comes out clean, about 15 minutes more.\n "
 	},
 ]
 
